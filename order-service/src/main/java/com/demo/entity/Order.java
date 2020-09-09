6e5f2c4acf9fd94136678d37b9f4542fd71e26d8
pxr26 <prakash.ramachandran@lennoxintl.com>
2020-09-09 21:41:15 +0530

    initial committ

---
+++ order-service/src/main/java/com/demo/entity/Order.java
@@ -0,0 +1,55 @@
+package com.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ORDER_TB")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    @Id
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    private String name;
+    private int qty;
+    private double price;
+}
---
+++ payment-service/src/main/java/com/demo/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.demo.repository;
+
+import com.demo.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment,Integer> {
+    Payment findByOrderId(int orderId);
+}
+
